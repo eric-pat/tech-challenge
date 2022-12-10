4f62cc2a7c984e9d6071d386eea7c0258d2e26b1
ERIC_PATROUILLAULT <ekcz@orange.fr>
2022-12-10 16:46:32 +0100

    End tech challenge

---
+++ script.js
@@ -0,0 +1,19 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('#castleTitle');
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+
+anime
+  .timeline({ loop: false })
+  .add({
+    targets: '#castleTitle .letter',
+    scale: [4, 1],
+    opacity: [0, 1],
+    translateZ: 0,
+    easing: 'easeOutExpo',
+    duration: 950,
+    delay: (el, i) => 70 * i,
+  })
+  
